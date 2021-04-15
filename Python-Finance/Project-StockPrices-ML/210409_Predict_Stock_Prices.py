ddf25a7a54bbb96f71970fb993fa01d7e6152b2f
kathuman <kathuman@gmail.com>
2021-04-15 08:56:04 +0200

    v

--- Python-Finance/Project-StockPrices-ML/210409_Predict_Stock_Prices.py
+++ Python-Finance/Project-StockPrices-ML/210409_Predict_Stock_Prices.py
@@ -35,7 +35,7 @@ x_train = []
 y_train = []
 
 for x in range(prediction_days,len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days]:x,0)
+    x_train.append(scaled_data[x-prediction_days:x,0])
     y_train.append(scaled_data[x,0])
     
 x_train, y_train = np.array(x_train), np.array(y_train)
@@ -55,3 +55,27 @@ model.add(Dense(units=1)) # Prediction of the next closing price
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+# Load the test data
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs =model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+# Make predictions on test data
+
+x_test =[]
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x,0])
+    
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (y_test.shape[0], x_test.shape[1],1))
+
+predicted
